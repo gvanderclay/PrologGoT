f79abd3c44c4f8e23080373e9671a7ec69d198fd
Gage Vander Clay <gvanderclay@yahoo.com>
2016-03-03 12:38:53 -0500

    add more rules

--- project2.pl
+++ project2.pl
@@ -4,7 +4,6 @@
 %male(character)
 %	Succeeds if the character is a male
 
-
 %female(character)
 %	Succeeds if the character is a female
 
@@ -13,7 +12,6 @@
 
 %baratheon(character)
 %	Succeeds if the character is part of the House Baratheon
-
 
 %lannister(character)
 %	Succeeds if the character is part of the House Lannister
@@ -27,7 +25,6 @@
 %tyrell(character)
 %	Succeeds if the character is part of the House Tyrell
 
-
 %targaryen(character)
 %	Succeeds if the character is part of the House Targaryen
 
@@ -39,4 +36,21 @@
 
 %nights_watch(character)
 %	Succeeds if the character is part of the Nights Watch
+
+%wildling(character)
+%	Succeeds if the character is a wildling
+
+%married(character1, character2)
+%	Succeeds if character1 is married to character2
+%	Need a definition for both directions
+%	e.g. 
+%	married('eddard stark', 'catelyn stark').
+%	married('catelyn stark', 'eddard stark').
+
+%serves(character, house)
+%	Succeeds if character is in service to the given house
+
+%bastard(character)
+%	Succeeds if the character is a bastard child
+
 
