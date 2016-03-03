b3d4c2740d432fc03bdc0907cecd4bdcd2347515
Gage Vander Clay <gvanderclay@yahoo.com>
2016-03-03 12:24:08 -0500

    make comments for rules involving families and the nights watch

---
+++ project2.pl
@@ -0,0 +1,42 @@
+%Rules
+
+
+%male(character)
+%	Succeeds if the character is a male
+
+
+%female(character)
+%	Succeeds if the character is a female
+
+%stark(character)
+%	Succeeds if the character is part of the House Stark
+
+%baratheon(character)
+%	Succeeds if the character is part of the House Baratheon
+
+
+%lannister(character)
+%	Succeeds if the character is part of the House Lannister
+
+%greyjoy(character)
+%	Succeeds if the character is part of the House Greyjoy
+
+%tully(character)
+%	Succeeds if the character is part of the House Tully
+
+%tyrell(character)
+%	Succeeds if the character is part of the House Tyrell
+
+
+%targaryen(character)
+%	Succeeds if the character is part of the House Targaryen
+
+%martell(character)
+%	Succeeds if the character is part of the House Martell
+
+%arryn(character)
+%	Succeeds if the character is part of the House Arryn
+
+%nights_watch(character)
+%	Succeeds if the character is part of the Nights Watch
+
